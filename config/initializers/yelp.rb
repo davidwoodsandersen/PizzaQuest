facca37903160b00698d34d37c542f4bc90cb574
Ian Andreu <ianandreu87@gmail.com>
2015-09-14 12:03:35 -0400

    remove test welcome index

--- config/initializers/yelp.rb
+++
@@ -1,6 +0,0 @@
-Yelp.client.configure do |config|
-  config.consumer_key = 'egl67CWi_sWWWdw3KcAd3w'
-  config.consumer_secret = 'JLxGfh9il54K6uVBX8RKSNL5NjU'
-  config.token = 'FGh6m_D0EZo6omRtXOBN_4TbMKKjO4l5'
-  config.token_secret = 'wraH59yFo0mkYLh2C4g4wLY0nIY'
-end
