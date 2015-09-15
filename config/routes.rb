df7db71b965cc32f36425781971b6096d38c6957
David Andersen <davidwoodsandersen@gmail.com>
2015-09-15 15:11:10 -0400

    fixes routing to new.erb

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get '/users/dashboard' => 'users#dashboard', as: :dashboard
   get '/users/profile' => 'users#profile', as: :profile
   get '/users/log_in' => 'users#log_in', as: :log_in
+  get '/users/new' => 'users#new'
   get '/users/:id' => 'users#show'
   resources :users, only: [:new, :create]
 
