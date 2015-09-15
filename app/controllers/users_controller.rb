fda1d9b0fcc7b16cc2231b9455a75d9359c07d07
Alan Rice <aricedrums@gmail.com>
2015-09-15 14:07:28 -0400

    tries to fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,6 +37,20 @@ class UsersController < ApplicationController
     end
 
 
+    def show
+      @user = User.find(params[:id])
+      respond_to do |format|
+        format.html # show.html.erb
+        format.json { render json: @user }
+      end
+    end
+
+    #update user profile info
+
+
+
+
+
     private
 
     def user_params
