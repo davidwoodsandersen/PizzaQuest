93a49265d086eb0cf43a7025567e85a7ee75f053
Alan Rice <aricedrums@gmail.com>
2015-09-15 14:34:40 -0400

    routes glitch

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,7 @@ Rails.application.routes.draw do
   get '/users/dashboard' => 'users#dashboard', as: :dashboard
   get '/users/profile' => 'users#profile', as: :profile
   get '/users/log_in' => 'users#log_in', as: :log_in
-  get '/users/:id' => 'users#show'
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create]
 
 
   post '/sessions' => 'sessions#create'
