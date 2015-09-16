81e63d071c9c7cbd5d741ac6c014204a6a3378cd
David Andersen <davidwoodsandersen@gmail.com>
2015-09-16 11:36:25 -0400

    adds pizza cat

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -82,6 +82,18 @@ $(document).ready(function(){
 
 
 
+  // Add an event to OPEN the modal
+  $("#open-modal").on("click", function() {
+    console.log("You clicked the open button!");
+    $("#modal").toggle();
+  });
+
+  // Add an event to CLOSE the modal
+  $("#close-modal").on("click", function() {
+    console.log("You clicked the close button.");
+    $("#modal").toggle();
+  });
+
 
 
 });
