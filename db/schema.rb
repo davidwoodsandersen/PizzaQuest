7aad65eb96ca7299cb6161e1fab7b7124c0bb95f
Bryan Fellerath <bfellerath@gmail.com>
2015-09-16 09:15:06 -0400

    resolving migration errors

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150915223811) do
+ActiveRecord::Schema.define(version: 20150915190605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,8 @@ ActiveRecord::Schema.define(version: 20150915223811) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "token"
-    t.string   "tagline"
     t.integer  "points_total"
+    t.string   "tagline"
   end
 
   create_table "visits", force: :cascade do |t|
