35593049eab09cede63239810b822db48e798956
Alan Rice <aricedrums@gmail.com>
2015-09-16 10:34:48 -0400

    adds points

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150915220424) do
+ActiveRecord::Schema.define(version: 20150915223811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20150915220424) do
     t.datetime "updated_at",      null: false
     t.string   "token"
     t.string   "tagline"
+    t.integer  "points_total"
   end
 
   create_table "visits", force: :cascade do |t|
